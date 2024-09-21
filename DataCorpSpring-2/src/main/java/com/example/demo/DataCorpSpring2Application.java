67340755c3435242b43294a36a49431072bd5e39
antonydass08 <153543597+antonydass08@users.noreply.github.com>
2024-09-21 10:46:13 +0530

    Add files via upload

---
+++ DataCorpSpring-2/src/main/java/com/example/demo/DataCorpSpring2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DataCorpSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCorpSpring2Application.class, args);
+	
+	}
+
+}
---
+++ DataCorpSpring-2/src/test/java/com/example/demo/DataCorpSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCorpSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
